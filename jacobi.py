11338b6560398c7e9e64920c4141e92010a8c325
David Hinojosa <64158375+dhinogz@users.noreply.github.com>
2021-03-03 11:30:48 -0600

    Clean jacobi.py

--- jacobi.py
+++ jacobi.py
@@ -4,31 +4,33 @@
 import numpy as np 
 
 # Matriz A (x, y, z)
-A = np.array([[5., -2., 3.,], [-3., 9., 1.], [2., -1., -7.]])
+#A = np.array([[5., -2., 3.,], [-3., 9., 1.], [2., -1., -7.]])
+A = np.array([[5., -1., 2.,], [-3., 8., -2.], [1., 1., 4.]])
 
 # Vector x (empieza en ceros)
 x = np.array([0., 0., 0.])
 #             x1  x2  x3
 
 # Vector b
-b = np.array([-1., 2., 3.])
+#b = np.array([-1., 2., 3.])
+b = np.array([12., -25., 6.])
 
 # numero de iteraciones
 N = 10                  
 
 # Iterar N cantidad de veces. En nuestro caso, son 10
 for i in range(N):
-    print("Iteracion "+ str(i+1) + ": " + str(x))
+    print("Iteracion "+ str(i) + ": " + str(x))
 
     # usaremos x_n para guardar nuestra respuesta cada iteracion
     x_n = np.zeros_like(x)
 
-    for j in range(A.shape[0]):
+    for j in range(b.shape[0]):
         # abajo de la diagonal
         diagonal1 = np.dot(A[j, :j], x[:j])
         # arriba de la diagonal
         diagonal2 = np.dot(A[j, j+1:], x[j+1:])
-
+        # Ax = b => x = b / A
         x_n[j] = (b[j] - diagonal1 - diagonal2) / A[j, j]
         
     x = x_n
